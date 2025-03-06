b5ed40e0b6936bf79e83f3064af3cc33d0f254f8
sainakashani <sainakashani1383@gmail.com>
2025-03-06 11:25:25 -0800

    hw1

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@ int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
     // <b>lang</b> variable name to see how CLion can help you rename it.
     auto lang = "C++";
+
     std::cout << "Hello and welcome to " << lang << "!\n";
 
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i <= 5;  i++) {
         // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
         // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
         // breakpoint for you, but you can always add more by pressing
